7dca5a471f975cfeb8c7c319a1d82e9965115f63
Ryong1998 <yoy0318@naver.com>
2022-07-22 11:54:11 +0900

    데이터베이스 연결 부분 수정

--- insert_data_test.py
+++ insert_data_test.py
@@ -1,10 +1,18 @@
 # 1. 라이브러리 가져오기
 import pymysql
-
+import datetime
 
 # 2. 접속하기 - 해당 데이터 베이스에 접속
 db = pymysql.connect(host='localhost', port=3306, user='root', passwd='yoy0317689*', db='snowball_database', charset='utf8') 
 
+
+# 기간동안의 모든 날짜들을 구하기 위한 함수
+date_list=list()
+date = datetime.datetime(2003,8,1,12,4,5)
+for i in range(5): 
+    date += datetime.timedelta(days=1)
+    date_list.append(str(date).split()[0])
+print(date_list) 
  
 
 # 3. 커서 가져오기 - #cursor 는 control structure of database로, ecommerce 대신 어떤 변수명으로 하든 상관없음 
@@ -19,7 +27,7 @@ sql = '''
 '''
 
 # 5. SQL 구문 실행하기 - sql 변수에 sql 명령어를 넣고 .execute()를 통해 실행
-snowball.execute(sql) 
+# snowball.execute(sql) 
 
  
 
@@ -32,7 +40,7 @@ snowball.execute(sql)
 
 # 잘못됬을 경우 복원등 용이하게 하기 위해서삽입, 갱신, 삭제 등이 모두 끝났으면 Connection 객체의 commit() 메서드를 사용하여 데이타를 Commit함. commit을 안하면 이전까지 작업한 것이 저장이 안될 수도 있음
 
-db.commit()
+# db.commit()
 
 
 # 7. DB 연결 닫기 - 사용한 후 연결끊기
---
+++ test.py
@@ -0,0 +1,3 @@
+# pip install 이 되지 않아서 경로 확인
+import sys
+print(sys.executable)
