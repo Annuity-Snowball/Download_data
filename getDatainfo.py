936c47077b25bfe912cf01454f7eedd1e67f692d
kiskikikuku <pthkuku99@gmail.com>
2022-07-23 18:25:14 +0900

    yyyy-mm-dd형태로 리스트에 저장

--- getDatainfo.py
+++ getDatainfo.py
@@ -7,12 +7,15 @@ def getDateInfo(start_date, end_date, interval):
     set = rruleset()
 
     if(interval == "first"):
-        return list(rrule(MONTHLY, byweekday=(MO,TU,WE,TH,FR), bysetpos=1, dtstart=parse(start_date), until=parse(end_date))) # 지정된 기간의 매월 첫 평일
+        lst =  list(rrule(MONTHLY, byweekday=(MO,TU,WE,TH,FR), bysetpos=1, dtstart=parse(start_date), until=parse(end_date))) # 지정된 기간의 매월 첫 평일
 
     else:
-        return list(rrule(MONTHLY, byweekday=(MO, TU, WE, TH, FR), bysetpos=-1, dtstart=parse(start_date), until=parse(end_date)))  # 지정된 기간의 매월 마지막 평일
+        lst =  list(rrule(MONTHLY, byweekday=(MO, TU, WE, TH, FR), bysetpos=-1, dtstart=parse(start_date), until=parse(end_date)))  # 지정된 기간의 매월 마지막 평일
+    rtList = []
+    for i in lst:
+        rtList.append(i.strftime('%Y-%m-%d')) 
 
-firstDate = getDateInfo("20220102T090000", "20221215T090000", "first")
-print(firstDate)
-lastDate = getDateInfo("20220102T090000", "20221215T090000", "last")
-print(lastDate)
+    print(rtList)
+
+firstDate = getDateInfo("20220102", "20221215", "first")
+# print(firstDate[0].strftime('%Y-%m-%d'))
