38f6573e3008199dbdc51720bc123e93e12933cb
TaeHyun Park <pthkuku99@gmail.com>
2022-07-31 14:24:37 +0900

    월초 월말 개장일 리스트 반환 함수 완성 - getPayInDateInfo

--- getDatainfo.py
+++ getDatainfo.py
@@ -1,18 +1,39 @@
-import dateutil.rrule
-from dateutil.relativedelta import *
 from dateutil.parser import *
+from dateutil.relativedelta import *
 from dateutil.rrule import *
+import exchange_calendars as ecal
+import pandas as pd
 
-def getDateInfo(start_date, end_date, interval):
-    set = rruleset()
+x = ecal.get_calendar("XKRX")  # 한국 증시 코드
 
-    if(interval == "first"):
-        return list(rrule(MONTHLY, byweekday=(MO,TU,WE,TH,FR), bysetpos=1, dtstart=parse(start_date), until=parse(end_date))) # 지정된 기간의 매월 첫 평일
+
+def getPayInDateInfo(start_date, end_date, interval):  # 납입일 계산 (월초 or 월말)
+    rtList = []
+    if interval == "first":
+        a = list(rrule(MONTHLY, byweekday=(MO, TU, WE, TH, FR),
+                       bysetpos=1,
+                       dtstart=parse(start_date),
+                       until=parse(end_date)))  # 지정된 기간의 매월 첫 평일 (월초)
+        for i in a:
+            if not x.is_session(i):  # 개장일이 아닌 날이 있는지 check
+                i = x.next_open(i)  # 다음 개장일
+            rtList.append(i.strftime('%Y-%m-%d'))  # yyyy-mm-dd 형식 변환
+        return rtList  # 납입 예정일 리스트 출력
 
     else:
-        return list(rrule(MONTHLY, byweekday=(MO, TU, WE, TH, FR), bysetpos=-1, dtstart=parse(start_date), until=parse(end_date)))  # 지정된 기간의 매월 마지막 평일
+        a = list(rrule(MONTHLY,
+                       byweekday=(MO, TU, WE, TH, FR),
+                       bysetpos=-1,
+                       dtstart=parse(start_date),
+                       until=parse(end_date)))  # 지정된 기간의 매월 마지막 평일
 
-firstDate = getDateInfo("20220102T090000", "20221215T090000", "first")
-print(firstDate)
-lastDate = getDateInfo("20220102T090000", "20221215T090000", "last")
-print(lastDate)
+        for i in a:
+            if not x.is_session(i):  # 개장일이 아닌 날이 있는지 check
+                i = x.previous_open(i)  # 직전 개장일
+            rtList.append(i.strftime('%Y-%m-%d'))  # yyyy-mm-dd 형식 변환
+        return rtList  # 납입 예정일 리스트 출력
+
+
+def dailyDateInfo(start_date, end_date):  # 지정한 기간 사이의 모든 개장일 반환
+
+    pass
