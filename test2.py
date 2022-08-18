3960650a149bbc992ccac812119e87fbc818c2aa
Ryong1998 <yoy0318@naver.com>
2022-08-18 14:10:01 +0900

    종목날짜 조회 파트수정

--- test2.py
+++ test2.py
@@ -2,13 +2,13 @@ import pandas as pd
 import getDatainfo
 from datetime import datetime
 
-df = pd.read_csv("ad.csv")
+df = pd.read_csv("C:\self_project\snowball\Download_data\\ad.csv")
 code_list = list(df['code'])
 date_list = list(df['date'])
 
-dailyDate_dict = dict()
+# dailyDate_dict = dict()
 payinDate_dict_bm = dict()
-payinDate_dict_lm = dict()
+# payinDate_dict_lm = dict()
 
 #for i in range(len(code_list)):
     #product_dict[code_list[i]] = date_list[i]
@@ -23,15 +23,7 @@ payinDate_dict_lm = dict()
 for i in range(len(code_list)):
     payinDate_dict_bm[code_list[i]] = getDatainfo.getPayInDateInfo(date_list[i], datetime.today().strftime('%Y-%m-%d'), 'first')
 
-print(payinDate_dict_bm)
-#
-""" for i in range(len(code_list)):
-    dailyDate_dict[code_list[i]] = getDailyDateInfo(date_list[i], datetime.today().strftime('%Y-%m-%d'))
-print(dailyDate_dict)
-"""
-
-for i in range(len(code_list)):
-    payinDate_dict_bm[code_list[i]] = getDatainfo.getPayInDateInfo(date_list[i], datetime.today().strftime('%Y-%m-%d'), 'first')
+# print(payinDate_dict_bm)
 
 # 매달 말 나오는것
 # for i in range(len(code_list)):
@@ -44,3 +36,6 @@ for stock_code in payinDate_dict_bm.keys():
     for search_date in payinDate_dict_bm[stock_code]:
         list1.append(stock_code)
         list2.append(search_date)
+print(list1)
+print()
+print(list2)
