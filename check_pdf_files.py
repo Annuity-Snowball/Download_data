8fee90df6580b6ced507de23ed379aa2f8c0558b
Ryong1998 <yoy0318@naver.com>
2022-08-29 11:22:28 +0900

    다운받은 파일 확인하는 코드 업데이트

--- check_pdf_files.py
+++ check_pdf_files.py
@@ -6,31 +6,48 @@ from datetime import datetime
 import os
 
 # 다운받은 파일들을 확인
-file_list = os.listdir('C:/Users/LG/Desktop/PDF_files')
+file_list = os.listdir('C:\\Users\\LG\\Desktop\\sangsu_file\\pdf_files')
 for i,file_name in enumerate(file_list):
-    file_list[i] = file_name.split('.')[0].split('_')
-print(file_list[:10])
+    file_list[i] = file_name.split('.')[0]
+print('다운받은 파일 개수 :', len(file_list))
 
     
 
+# 다운 받았어야 하는 파일들 확인
+df = pd.read_csv("C:\self_project\snowball\Download_data\\ad.csv")
+code_list = list(df['code'])
+date_list = list(df['date'])
 
-# df = pd.read_csv("C:\self_project\snowball\Download_data\\ad.csv")
-# code_list = list(df['code'])
-# date_list = list(df['date'])
+payinDate_dict_bm = dict()
 
-# payinDate_dict_bm = dict()
+# 매달 초 나오는 것
+for i in range(len(code_list)):
+    payinDate_dict_bm[code_list[i]] = getDatainfo.getPayInDateInfo(date_list[i], datetime.today().strftime('%Y-%m-%d'), '0')
 
-# # 매달 초 나오는 것
-# for i in range(len(code_list)):
-#     payinDate_dict_bm[code_list[i]] = getDatainfo.getPayInDateInfo(date_list[i], datetime.today().strftime('%Y-%m-%d'), 'first')
-
-# list1 = []
-# list2 = []
-# for stock_code in payinDate_dict_bm.keys():
-#     for search_date in payinDate_dict_bm[stock_code]:
-#         list1.append(stock_code)
-#         list2.append(search_date)
+list1 = []
+list2 = []
+for stock_code in payinDate_dict_bm.keys():
+    for search_date in payinDate_dict_bm[stock_code]:
+        list1.append(stock_code)
+        search_date = ''.join(search_date.split('-'))
+        list2.append(search_date)
         
-        
-# print(list1[:10])
-# print(list2[:10])
+print('list1 lengh :',len(list1))
+print('list2 lengh :',len(list2))
+
+correct_list = list() # 다운 받았어야 하는 파일
+
+for i in range(len(list1)):
+    correct_list.append(str(list1[i])+'_'+list2[i])
+
+print('다운받았어야 하는 파일 개수 :', len(correct_list))
+
+missing_file = list()
+for filename in correct_list:
+    if filename in file_list:
+        pass
+    else:
+        missing_file.append(filename)
+    
+print('다운 못받은 파일 개수 :',len(missing_file))
+print(missing_file)
