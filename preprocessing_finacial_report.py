ae64f1e4f7167f44220ca8362ccb74086281c244
Ryong1998 <yoy0318@naver.com>
2022-09-16 23:22:25 +0900

    파일 경로 수정

--- preprocessing_finacial_report.py
+++ preprocessing_finacial_report.py
@@ -5,7 +5,7 @@ from IPython.display import display
 import csv
 
 pdf_stock_list = list() # pdf에서 사용되는 종목코드들이 담겨 있는 리스트 생성
-with open('C:\\self_project\\snowball\\Download_data\\need_stock_list.csv', newline='', encoding='utf-8') as csvfile:
+with open('C:\\self_project\\snowball\\Download_data\\pdf_stock_list.csv', newline='', encoding='utf-8') as csvfile:
     pre_pdf_stock_list = list(csv.reader(csvfile, delimiter=' ')) # need_stock_list.csv 파일에서 종목코드들을 불러옴
 
 
